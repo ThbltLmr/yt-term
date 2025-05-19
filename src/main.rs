747486a347973bfc4682b831502d5079aa2d3aa0
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-05-19 23:54:58 +0200

    stock

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, process::Command};
 
 use clap::Parser;
 
@@ -44,6 +44,38 @@ impl VideoBuffer {
 struct Args {
     #[clap(short, long)]
     url: String,
+
+    #[clap(short, long, default_value = "247")]
+    format: String,
 }
 
-fn main() {}
+fn main() {
+    let args = Args::parse();
+    let url = args.url;
+
+    let mut yt_dlp_process = Command::new("yt-dlp")
+        .args([
+            "-o",
+            "-",         // Output to stdout
+            "--no-part", // Don't create .part files
+            "-f",
+            "best", // Choose format
+            &url,
+        ])
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    // Connect yt-dlp's stdout to ffmpeg's stdin
+    let yt_dlp_stdout = yt_dlp_process.stdout.take().unwrap();
+
+    let mut ffmpeg_process = Command::new("ffmpeg")
+        .args([
+            "-i", "pipe:0", // Read from stdin
+            "-f", "rawvideo", "-pix_fmt", "rgb24",
+            // Other options...
+            "-", // Output to stdout
+        ])
+        .stdin(Stdio::from(yt_dlp_stdout)) // Connect the pipes
+        .stdout(Stdio::piped())
+        .spawn()?;
+}
