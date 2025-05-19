8d7d48da9ef9034c4f2a088ec600b41e0ed47b83
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-05-19 21:32:27 +0200

    feat: remove yt utils and switch to pipe approach

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,49 @@
+use std::collections::VecDeque;
+
+use clap::Parser;
+
 mod result;
-mod yt_utils;
+
+const MAX_BUFFER_SIZE: usize = 100;
+
+struct VideoFrame {
+    data: Vec<u8>,
+    timestamp: u64,
+}
+
+struct VideoBuffer {
+    frames: VecDeque<VideoFrame>,
+}
+
+impl VideoBuffer {
+    fn new() -> Self {
+        VideoBuffer {
+            frames: VecDeque::with_capacity(MAX_BUFFER_SIZE),
+        }
+    }
+
+    fn push_frame(&mut self, frame: VideoFrame) {
+        if self.frames.len() >= MAX_BUFFER_SIZE {
+            // If buffer is full, remove the oldest frame
+            self.frames.pop_front();
+        }
+        self.frames.push_back(frame);
+    }
+
+    fn get_frame(&mut self) -> Option<VideoFrame> {
+        self.frames.pop_front()
+    }
+
+    fn len(&self) -> usize {
+        self.frames.len()
+    }
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(short, long)]
+    url: String,
+}
 
 fn main() {}
