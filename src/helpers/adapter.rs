176056e2a0be31a17395ee4bbd8d8aee25b2fcf2
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-07-15 10:17:33 -0400

    refacto: remove mutex for adapter trait

--- src/helpers/adapter.rs
+++ src/helpers/adapter.rs
@@ -1,5 +1,5 @@
 use std::{
-    sync::{mpsc::Receiver, Arc, Mutex},
+    sync::mpsc::Receiver,
     thread,
     time::{Duration, Instant},
     usize,
@@ -11,20 +11,20 @@ use super::{
 };
 
 pub trait Adapter {
-    fn new(buffer: Arc<Mutex<ContentQueue>>, producer_done_rx: Receiver<()>) -> Res<Self>
+    fn new(buffer: ContentQueue, producer_done_rx: Receiver<()>) -> Res<Self>
     where
         Self: Sized;
 
     fn process_element(&self, element: BytesWithTimestamp) -> Res<()>;
 
-    fn get_buffer(&self) -> Arc<Mutex<ContentQueue>>;
+    fn get_buffer(&self) -> ContentQueue;
 
     fn is_buffer_empty(&self) -> bool {
-        self.get_buffer().lock().unwrap().is_empty()
+        self.get_buffer().is_empty()
     }
 
     fn get_buffer_element(&self) -> Option<BytesWithTimestamp> {
-        self.get_buffer().lock().unwrap().get_el()
+        self.get_buffer().get_el()
     }
 
     fn is_producer_done(&self) -> bool;
